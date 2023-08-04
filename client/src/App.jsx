7df3a2c37f24bc5f82173e45cf8775879c31b796
Jony Bhattacharjee <jonybhattacharjee886@gmail.com>
2023-08-04 14:28:59 +0600

    .gitignore change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-	return <div>App</div>;
+	return (
+		<h1 className="" style={{ fontWeight: "bold", fontSize: "35px" }}>
+			App
+		</h1>
+	);
 }
 
 export default App;
